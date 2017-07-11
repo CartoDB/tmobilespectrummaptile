b24bff937ccbe648551d78c3ae23e4f42e436192
nygeog <daniel.martin.sheehan@gmail.com>
2017-07-11 10:21:57 -0400

    Update

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ def urlChange(inURL):
     z = int(inURL.split('/')[slashes - 2].split('/')[0])
     x = int(inURL.split('/')[slashes - 1].split('/')[0])
     y = int(inURL.split('/')[slashes].split('.png')[0])
-    print z, x, y
     z = z + 1
     x = x + 1
     y = y + 1
@@ -32,6 +31,3 @@ def anypage(page):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-# 1. use regexes to parse your url, find the z, x, and y, and add +1s
-# 2. come up with a new url structure that maybe has the z, x, and y as query string parameters or path parameters and have flask interpret those to add +1s and ask tmobile for the tile
